1a6875b1ff8d8164188a2b5886e306fe5233eafd
Qabil <leancyleancy5@gmail.com>
2026-05-29 20:06:47 +0700

    Fixing path server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ mongoose.connect(process.env.MONGODB_URI, {
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
 
 // Session
 app.use(session({
@@ -46,12 +46,12 @@ app.use('/api/admin', require('./routes/admin'));
 
 // Main page
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Admin page
 app.get('/admin', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'admin.html'));
+  res.sendFile(path.join(__dirname, 'admin.html'));
 });
 
 // Health check
